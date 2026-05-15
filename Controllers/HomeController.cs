5bb2108f1b8e9b30647d7b3a17e0d8e67c43b30a
JAZMIN AGOSTINA HAN <50159035@ort>
2026-05-15 08:48:29 -0300

    terminadito

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,22 +18,25 @@ public class HomeController : Controller
         return View();
     }
     public IActionResult GenerarSugerencia(SugeridorReceta datos)
-    {
-        int edad = datos.CalcularEdad(datos.fechaNacimiento);
-        string plato = datos.DeterminarPlato(datos.tipoComida, datos.presupuesto);
-        double tiempo = datos.CalcularTiempo(datos.tipoComida, datos.cantComensales);
-        string dificultad = datos.DeterminarDificultad(datos.presupuesto, datos.cantComensales);
+{
+    int edad = datos.CalcularEdad(datos.fechaNacimiento);
+    string platoSugerido = datos.DeterminarPlato(datos.tipoComida, datos.presupuesto);
+    double tiempo = datos.CalcularTiempo(datos.tipoComida, datos.cantComensales);
+    string dificultad = datos.DeterminarDificultad(datos.presupuesto, datos.cantComensales);
 
-      
-        ViewBag.Nombre = datos.nombre;
-        ViewBag.Edad = edad;
-        ViewBag.Plato = plato;
-        ViewBag.Tiempo = tiempo;
-        ViewBag.Dificultad = dificultad;
-        ViewBag.CantComensales = datos.cantComensales;
-        ViewBag.Presupuesto = datos.presupuesto;
-        return View("Resultado");
-    }
+    ViewBag.nombre = datos.nombre;
+    ViewBag.edad = edad;
+    ViewBag.platoSugerido = platoSugerido;
+    ViewBag.tiempo = tiempo;
+    ViewBag.dificultad = dificultad;
+    ViewBag.cantComensales = datos.cantComensales;
+    ViewBag.presupuesto = datos.presupuesto;
+
+    ViewBag.fechaNacimiento = datos.fechaNacimiento;
+    ViewBag.tipoComida = datos.tipoComida;
+
+    return View("Resultado");
+}
     public IActionResult Privacy()
     {
         return View();
